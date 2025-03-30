e3be0e501392ec6584b7225f47221fac8cc721f3
25Jburton <25jburton@gmail.com>
2025-03-30 19:25:12 -0400

    Paddle movement and gameOver features added

--- javaScript/brick_breaker/functions.js
+++ javaScript/brick_breaker/functions.js
@@ -1,21 +1,81 @@
 function drawBall() {
     // Create the circle 
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = "green";
     ctx.fill();
     ctx.closePath();
 }
 
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "green";
+    ctx.fill();
+    ctx.closePath();
+}  
+
 function draw() {
     // Show once, remove the path
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    drawPaddle();
     x += dx;
     y += dy;
+    // Set left and right boundaries
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    // Set top and bottom boundaries
+    if (y + dy < ballRadius) {
+        dy = -dy;
+    }else if(y + dy > canvas.height - ballRadius){
+        if (x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+        } else {
+            canvas.style.display = "none";
+            gameOver.style.display = "block";
+            restartBtn.style.display = "block";
+        }
+    }
+    x += dx;
+    y += dy;
+
+    // Paddle movement
+    if (rightPressed) {
+        paddleX = Math.min(paddleX + 7, canvas.width - paddleWidth);
+    } else if (leftPressed) {
+        paddleX = Math.max(paddleX - 7, 0);
+    }
+      
+      
+}
+
+function keyDownHandler(e) {
+    if (e.key === "Right" || e.key === "ArrowRight" || e.key === 'd' || e.key === 'D') {
+      rightPressed = true;
+    } else if (e.key === "Left" || e.key === "ArrowLeft" || e.key === 'a' || e.key === 'A') {
+      leftPressed = true;
+    }
+  }
+  
+  function keyUpHandler(e) {
+    if (e.key === "Right" || e.key === "ArrowRight" || e.key === 'd' || e.key === 'D') {
+      rightPressed = false;
+    } else if (e.key === "Left" || e.key === "ArrowLeft" || e.key === 'a' || e.key === 'A') {
+      leftPressed = false;
+    }
+  }
+
+function restartGame() {
+    document.location.reload();
+    clearInterval(interval);
 }
 
 function startGame() {
+    // Add paddle controls
+    document.addEventListener("keydown", keyDownHandler, false);
+    document.addEventListener("keyup", keyUpHandler, false);
     // Call the draw every 10 milliseconds
-    setInterval(draw,10);
+    interval = setInterval(draw, 10);
 }
--- javaScript/brick_breaker/main.js
+++ javaScript/brick_breaker/main.js
@@ -1,14 +1,29 @@
 // Setup Canvas
 const canvas = document.getElementById("game-board");
 const ctx = canvas.getContext("2d");
-
+const gameOver = document.getElementById("game-over");
+const restartBtn = document.getElementById("restart-button");
 let x = canvas.width / 2;
 let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
-
+let interval = 0;
+// Paddle vars
+const paddleHeight = 10;
+const paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth) / 2;
+// User Control vars
+let rightPressed = false;
+let leftPressed = false;
+// Ball vars
+const ballRadius = 10;
 // Basic start game trigger
-document.getElementById("startButton").addEventListener("click", function () {
-    startGame();
-    this.disabled = true;
-  });
+document.getElementById("start-button").addEventListener("click", function () {
+  startGame();
+  this.disabled = true;
+});
+
+// Basic restart game trigger
+document.getElementById("restart-button").addEventListener("click", function () {
+  restartGame();
+});
